55ee0a903672965d5c8bdf5543b4364c157aecab
Dhruv Shah <dhruv5995@gmail.com>
2022-12-06 16:48:13 -0500

    deploy.py finished
    Signed-off-by: Dhruv Shah <dhruv.shah@ibm.com>

--- mlflow_watsonml/deploy.py
+++ mlflow_watsonml/deploy.py
@@ -52,7 +52,14 @@ def run_local(name, model_uri, flavor=None, config=None):
 
 
 class WatsonMLDeploymentClient(BaseDeploymentClient):
-    def __init__(self, target_uri):
+    def __init__(self, target_uri: str):
+        """initialize a WML deployment client
+
+        Parameters
+        ----------
+        target_uri : str
+            target uri: "watsonml"
+        """
         super().__init__(target_uri)
 
         self.wml_config = Config()
@@ -95,7 +102,11 @@ class WatsonMLDeploymentClient(BaseDeploymentClient):
         flavor: Optional[str] = None,
         config: Optional[Dict] = None,
     ) -> Dict:
-        """Deploy the model at `model_uri` to a WML target
+        """Deploy a model at `model_uri` to a WML target. this method blocks until
+        deployment completes (i.e. until it's possible to perform inference with the deployment).
+        In the case of conflicts (e.g. if it's not possible to create the specified deployment
+        without due to conflict with an existing deployment), raises a
+        :py:class:`mlflow.exceptions.MlflowException`.
 
         Parameters
         ----------
@@ -165,56 +176,88 @@ class WatsonMLDeploymentClient(BaseDeploymentClient):
         return deployment_details
 
     def delete_deployment(self, name: str, config: Optional[Dict] = None) -> None:
-        """Delete the deployment at the provided `name` from WML
+        """Delete the deployment with name ``name`` from WML. Deletion is idempotent
+        (i.e. deletion does not fail if retried on a non-existent deployment).
+
 
         Parameters
         ----------
         name : str
-            name of the deployment
+            name of the deployment to delete
         config : Optional[Dict], optional
             configuration parameters, by default None
         """
         client = self.get_wml_client()
-        if deployment_exists(client=client, name=name) or model_exists(
-            client=client, name=name
-        ):
+
+        if deployment_exists(client=client, name=name):
+            delete_deployment(client=client, name=name)
+
+        if model_exists(client=client, name=name):
             delete_model(client=client, name=name)
 
     def update_deployment(
-        self, name, model_uri=None, flavor=None, config=None, endpoint=None
+        self,
+        name: str,
+        model_uri: Optional[str] = None,
+        flavor: Optional[str] = None,
+        config: Optional[Dict] = None,
     ):
-        return super().update_deployment(name, model_uri, flavor, config, endpoint)
-
-    def predict(self, deployment_name=None, df=Optional[pd.DataFrame]) -> pd.DataFrame:
-        """_summary_
+        """Update the deployment with the specified name. You can update the URI of the model, the
+        flavor of the deployed model (in which case the model URI must also be specified), and/or
+        any WML-specific attributes of the deployment (via `config`). By default, this method
+        blocks until deployment completes (i.e. until it's possible to perform inference
+        with the updated deployment).
 
         Parameters
         ----------
-        deployment_name : _type_, optional
-            _description_, by default None
-        df : _type_, optional
-            _description_, by default Optional[pd.DataFrame]
+        name : str
+            Unique name of the deployment to update
+        model_uri : Optional[str], optional
+            URI of a new model to deploy, by default None
+        flavor : Optional[str], optional
+            new model flavor to use for deployment. If provided,
+            ``model_uri`` must also be specified. If ``flavor`` is unspecified but
+            ``model_uri`` is specified, a default flavor will be chosen and the
+            deployment will be updated using that flavor., by default None
+        config : Optional[Dict], optional
+            dict containing updated WML-specific configuration for the
+            deployment, by default None
+        """
+        self.delete_deployment(name=name, config=config)
+        self.create_deployment(
+            name=name, model_uri=model_uri, flavor=flavor, config=config
+        )
+
+    def predict(
+        self, deployment_name: str, inputs=Optional[pd.DataFrame]
+    ) -> pd.DataFrame:
+        """Compute predictions on inputs using the specified deployment
+
+        Parameters
+        ----------
+        deployment_name : str
+            Name of deployment to predict against
+        inputs : pd.DataFrame, optional
+            Input data (or arguments) to pass to the deployment for inference,
+            by default Optional[pd.DataFrame]
 
         Returns
         -------
         pd.DataFrame
-            _description_
-
-        Raises
-        ------
-        MlflowException
-            _description_
+            Model predictions as pandas.DataFrame
         """
         if self._wml_client is None:
             raise MlflowException(
-                message="Deployment doesn't exist. Call `create_deployment()`",
+                message="WML client is not initialized. Call :py:method:`connect`",
                 error_code=ENDPOINT_NOT_FOUND,
             )
 
         client = self.get_wml_client()
 
         scoring_payload = {
-            self._wml_client.deployments.ScoringMetaNames.INPUT_DATA: [{"values": df}]
+            self._wml_client.deployments.ScoringMetaNames.INPUT_DATA: [
+                {"values": inputs}
+            ]
         }
 
         deployment_id = get_deployment_id_from_deployment_name(
@@ -237,18 +280,44 @@ class WatsonMLDeploymentClient(BaseDeploymentClient):
         return ans
 
     def list_deployments(self) -> List[Dict]:
+        """List deployments. This method returns an unpaginated list of all deployments
+
+        Returns
+        -------
+        List[Dict]
+            A list of dicts corresponding to deployments. Each dict is guaranteed to
+            contain a 'name' key containing the deployment name. The other fields of
+            the returned dictionary and their types follow WML deployment details convention.
+        """
         return list_deployments(client=self.get_wml_client())
 
     def get_deployment(self, name: str) -> Dict:
+        """Returns a dictionary describing the specified deployment, throwing a
+        :py:class:`mlflow.exceptions.MlflowException` if no deployment exists with the provided
+        name.
+        The dict is guaranteed to contain a 'name' key containing the deployment name.
+        The other fields of the returned dictionary and their types follow WML convention.
+
+        Parameters
+        ----------
+        name : str
+            name of the deployment to fetch
+
+        Returns
+        -------
+        Dict
+            A dict corresponding to the retrieved deployment. The dict is guaranteed to
+            contain a 'name' key corresponding to the deployment name.
+        """
         return get_deployment(client=self.get_wml_client(), name=name)
 
     def get_wml_client(self) -> APIClient:
-        """_summary_
+        """Returns the WML API client
 
         Returns
         -------
         APIClient
-            _description_
+            WML client
         """
         if hasattr(self, "_wml_client"):
             return self._wml_client
