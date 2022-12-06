45f7d3cdbfee1556f1ad883c03495de18f9197be
Dhruv Shah <dhruv5995@gmail.com>
2022-12-06 17:31:47 -0500

    finished wml.py

--- mlflow_watsonml/wml.py
+++ mlflow_watsonml/wml.py
@@ -14,32 +14,27 @@ def store_model(
     model_description: str,
     model_type: str,
 ) -> Dict:
-    """_summary_
+    """Store model_object in a WML repository
 
     Parameters
     ----------
     client : APIClient
-        _description_
+        WML client
     model_object : Any
-        _description_
+        artifact object
     software_spec_uid : str
-        _description_
+        uid of software specification
     name : str
-        _description_
+        name of the deployment
     model_description : str
-        _description_
+        model description
     model_type : str
-        _description_
+        type of model
 
     Returns
     -------
     Dict
-        _description_
-
-    Raises
-    ------
-    MlflowException
-        _description_
+        model details dictionary
     """
     model_props = {
         client.repository.ModelMetaNames.NAME: name,
@@ -64,24 +59,27 @@ def store_model(
     return model_details
 
 
-def deploy_model(client: APIClient, name: str, model_id: str, batch: bool = False):
-    """_summary_
+def deploy_model(
+    client: APIClient, name: str, model_id: str, batch: bool = False
+) -> Dict:
+    """Create a new WML deployment
 
     Parameters
     ----------
     client : APIClient
-        _description_
+        WML client
     name : str
-        _description_
+        name of the new deployment to create
     model_id : str
-        _description_
+        UID of the model stored in WML repository
     batch : bool, optional
-        _description_, by default False
+        whether to use batch or online method of deployment,
+        by default False
 
     Returns
     -------
-    _type_
-        _description_
+    Dict
+        deployment details dictionary
     """
 
     if batch:
@@ -111,18 +109,42 @@ def deploy_model(client: APIClient, name: str, model_id: str, batch: bool = Fals
     return deployment_details
 
 
+def delete_deployment(client: APIClient, name: str):
+    """Delete an existing deployment from WML
+
+    Parameters
+    ----------
+    client : APIClient
+        WML client
+    name : str
+        name of the deployment to delete
+    """
+    try:
+        deployment_id = get_deployment_id_from_deployment_name(
+            client=client, deployment_name=name
+        )
+        client.deployments.delete(deployment_uid=deployment_id)
+    except Exception as e:
+        raise MlflowException(e)
+
+
 def delete_model(client: APIClient, name: str):
-    deployment_id = get_deployment_id_from_deployment_name(
-        client=client, deployment_name=name
-    )
-    client.deployments.delete(deployment_uid=deployment_id)
+    """Delete a model from WML repository
 
-    model_id = get_model_id_from_model_name(client=client, model_name=name)
-    client.repository.delete(artifact_uid=model_id)
+    Parameters
+    ----------
+    client : APIClient
+        WML client
+    name : str
+        name of the deployment to delete
+    """
+    try:
+        model_id = get_model_id_from_model_name(client=client, model_name=name)
+        client.repository.delete(artifact_uid=model_id)
 
-    # except Exception as e:
-    #     raise MlflowException(e)
+    except Exception as e:
+        raise MlflowException(e)
 
 
 def update_model(client: APIClient):
-    ...
+    raise NotImplementedError()
