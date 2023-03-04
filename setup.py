cc0083952d98c5ef0495188314a2f2d956c123c4
Dhruv Shah <dhruv5995@gmail.com>
2023-03-04 13:29:53 -0500

    update setup files
    Signed-off-by: Dhruv Shah <dhruv5995@gmail.com>

--- setup.py
+++ setup.py
@@ -3,13 +3,26 @@ from setuptools import find_packages, setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("VERSION", "r") as fh:
+    version = fh.read().strip()
+
 setup(
     name="mlflow-watsonml",
-    version="0.0.1",
+    version=version,
     description="WatsonML MLflow deployment plugin",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/IBM/mlflow-watsonml",
-    packages=find_packages(),
+    package_dir={"": "mlflow_watsonml"},
+    packages=find_packages(where="mlflow_watsonml"),
+    author="IBM AI Model Factory team",
+    author_email="dhruv.shah@ibm.com",
+    install_requires=[
+        "mlflow",
+        "ibm_watson_machine_learning",
+        "python_dotenv",
+        "joblib",
+    ],
     entry_points={"mlflow.deployments": "watsonml=mlflow_watsonml.deploy"},
+    python_requires=">=3.9",
 )
