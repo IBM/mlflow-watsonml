e8d02b2d2045da69e63058a11f8bfe2f004baaad
Dhruv Shah <dhruv.shah@ibm.com>
2023-11-27 19:24:36 +0000

    release baash script
    Signed-off-by: Dhruv Shah <dhruv.shah@ibm.com>

---
+++ release.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+VERSION=$1
+ROOT="$( cd "$(dirname "$0")" ; pwd -P )"
+
+if [ -z $VERSION ]; then
+    echo "usage: bash release <version>"
+    exit
+fi
+
+git checkout mlflow_watsonml/__init__.py
+if ! git diff-index --quiet HEAD --; then echo "can't create release, you have uncommitted files"; exit; fi
+if git status --porcelain 2>/dev/null | egrep "^ M|??"; then echo "Can't create release, you have uncommitted or untracked files"; exit; fi
+
+# make sure this tag is in our setup.py
+if ! grep version\=\"$VERSION\" setup.py; then "$VERSION does not match that listed in setup.py"; exit ; fi
+
+echo creating mlflow_watsonml release $VERSION
+echo "# THIS IS AN AUTOMATICALLY GENERATED FILE" > mlflow_watsonml/__init__.py
+echo "# DO NOT EDIT IT SINCE YOUR CHANGES WILL"  >> mlflow_watsonml/__init__.py
+echo "# BE LOST" >> mlflow_watsonml/__init__.py
+echo "__version__ = '$VERSION'" >> mlflow_watsonml/__init__.py
+
+git commit -m"release $VERSION" mlflow_watsonml/__init__.py
+
+cd $ROOT
+echo "creating tag and pushing"
+git tag -a $VERSION -m"new release" \
+ && git push origin $VERSION \
+ &&  git push
